dbd0bfb13b8e79e553046318a171f5a858bd067e
gamz <gamzabaw@gmail.com>
2020-10-04 22:30:42 +0900

    Add react-bootstrap

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,12 @@
 import App, { AppInitialProps, AppContext } from 'next/app'
 import { wrapper } from '../redux/store'
-import '../styles/index.css'
+
+import 'bootstrap/dist/css/bootstrap.min.css'
+import '../styles/custom.css'
 
 import _adapter from 'webrtc-adapter'
-import axios from 'axios'
 
+import axios from 'axios'
 axios.defaults.baseURL = 'http://localhost:3000'
 axios.defaults.headers.post['Content-Type'] = 'application/json'
 
