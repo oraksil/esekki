b3fde24b9022f8779558e170ec1b46dee3c6aab7
gamz <gamzabaw@gmail.com>
2020-10-22 23:18:14 +0900

    Wrap app with provider to fetch player every refresh

--- redux/common/actions.ts
+++ redux/common/actions.ts
@@ -5,6 +5,8 @@ import { Jsend } from '../../types/jsend'
 import { Game, Player, Joinable } from '../../types/game'
 
 import {
+  GET_PLAYER_OK,
+  GET_PLAYER_FAILED,
   NEW_PLAYER_OK,
   NEW_PLAYER_FAILED,
   START_NEW_GAME_OK,
@@ -12,6 +14,33 @@ import {
   CAN_JOIN_GAME_OK,
   CAN_JOIN_GAME_FAILED
 } from './types'
+
+const getPlayerOk = (player: Player) => {
+  return {
+    type: GET_PLAYER_OK,
+    payload: player
+  }
+}
+
+const getPlayerFailed = (player: Player) => {
+  return {
+    type: GET_PLAYER_FAILED,
+    payload: player
+  }
+}
+
+export const getPlayer = () => (dispatch: Dispatch) => {
+  axios.get(`/api/v1/players/me`)
+    .then(res => {
+      const jsend: Jsend = res.data
+      if (jsend.status === 'success') {
+        dispatch(getPlayerOk(jsend.data))
+      } else {
+        dispatch(getPlayerFailed(jsend.data))
+      }
+    })
+    .catch(reject => { dispatch(getPlayerFailed(reject)) })
+}
 
 const newPlayerOk = (player: Player) => {
   return {
@@ -29,7 +58,14 @@ const newPlayerFailed = (reject: any) => {
 
 export const newPlayer = (name: string) => (dispatch: Dispatch) => {
   axios.post(`/api/v1/players/new`, { name })
-    .then(res => { dispatch(newPlayerOk(res.data.data)) })
+    .then(res => { 
+      const jsend: Jsend = res.data
+      if (jsend.status === 'success') {
+        dispatch(newPlayerOk(jsend.data))
+      } else {
+        dispatch(newPlayerFailed(jsend.data))
+      }
+    })
     .catch(reject => { dispatch(newPlayerFailed(reject)) })
 }
 
@@ -52,7 +88,10 @@ export const canJoinGame = (gameId: number) => (dispatch: Dispatch) => {
     .then(res => { 
       const jsend: Jsend = res.data
       if (jsend.status === 'success') {
-        dispatch(canJoinGameOk(res.data.data))
+        const joinable: Joinable = { ...jsend.data, game: { id: gameId }}
+        dispatch(canJoinGameOk(joinable))
+      } else {
+        dispatch(canJoinGameFailed(jsend.data))
       }
     })
     .catch(reject => { dispatch(canJoinGameFailed(reject)) })
@@ -78,6 +117,8 @@ export const startNewGame = (packId: number) => (dispatch: Dispatch) => {
       const jsend: Jsend = res.data
       if (jsend.status === 'success') {
         dispatch(startNewGameOk(jsend.data))
+      } else {
+        dispatch(startNewGameFailed(jsend.data))
       }
     })
     .catch(reject => { dispatch(startNewGameFailed(reject)) })
