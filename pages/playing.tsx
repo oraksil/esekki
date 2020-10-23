e218cce3afe13f2dc24a81b097d2765ef5984934
gamz <gamzabaw@gmail.com>
2020-10-23 14:46:19 +0900

    Update prettierrc

--- pages/playing.tsx
+++ pages/playing.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { useEffect, useState, useRef } from 'react'
+import { useEffect, useState } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import { useRouter } from 'next/router'
 import Head from 'next/head'
@@ -28,7 +28,7 @@ type PlayerRect = {
 const setupResizeHandler = (playerRectSetter: any) => {
   const calculatePlayerRect = (windowHeight: number): PlayerRect => {
     const marginTop = windowHeight * PLAYER_PADDING_TOP_RATIO
-    const height = windowHeight * PLAYER_HEIGHT_RATIO 
+    const height = windowHeight * PLAYER_HEIGHT_RATIO
     const width = height * PLAYER_ASPECT_RATIO
     return { marginTop, width, height }
   }
@@ -43,7 +43,7 @@ const setupKeyHandler = () => {
   const handleKeyInput = (evt: any) => {
     WebRTCSession.sendKeyInput(evt.which | evt.key, evt.type === 'keydown')
   }
-  
+
   document.addEventListener('keyup', handleKeyInput)
   document.addEventListener('keydown', handleKeyInput)
 }
@@ -57,21 +57,21 @@ const extractGameId = (query: any): number | null => {
 
 ////////////////////////////////////////////////////////////////////////////////
 // Design Concept
-// 
+//
 // Assumption:
-// - For both new and joining game, 
+// - For both new and joining game,
 //   url looks like /playing?g=123 (g={gameId})
 //   it makes sense because there is an unique orakki.
 //
 // - New game has taken place already before landing here.
 //   (it might need registration form if player not found.)
-//   
+//
 // - Player session can exists or not exists.
 //
 // Flow:
 //   1. If no player found, provide registration form and create a new player.
 //      Once creating a player, we can start joining game.
-//      
+//
 //   2. Now player exists and we should check to be able to join the game.
 //
 //   3. If ok, let's go set up webrtc session and play.
@@ -86,7 +86,9 @@ const Playing = () => {
 
   const player = useSelector((state: RootState) => state.common.player)
   const game = useSelector((state: RootState) => state.common.game)
-  const streamOpen = useSelector((state: RootState) => state.webrtc.mediaStreamOpen)
+  const streamOpen = useSelector(
+    (state: RootState) => state.webrtc.mediaStreamOpen
+  )
 
   useEffect(() => {
     setupResizeHandler(setPlayerRect)
@@ -101,10 +103,10 @@ const Playing = () => {
       return
     }
 
-    // To keep 
+    // To keep
     // const gameId = extractGameId(router.query)
     // if (!game.joinToken && gameId) {
-      // dispatch(canJoinGame(gameId))
+    // dispatch(canJoinGame(gameId))
     // }
   }, [player])
 
@@ -138,7 +140,10 @@ const Playing = () => {
           </div>
         </div>
       </div>
-      <PlayerRegisterModal show={modalShow} onHide={() => setModalShow(false)} />
+      <PlayerRegisterModal
+        show={modalShow}
+        onHide={() => setModalShow(false)}
+      />
     </Layout>
   )
 }
@@ -146,7 +151,7 @@ const Playing = () => {
 export default Playing
 
 // export const getStaticProps = wrapper.getStaticProps(async ({ store }) => {
-  // return {
-    // props: {},
-  // }
+// return {
+// props: {},
+// }
 // })
