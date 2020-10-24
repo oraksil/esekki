0d99020f0f1062a1dc2a837dee7235f823bffb19
wonjae lee <onejae@gmail.com>
2020-10-24 19:48:02 +0900

    Use react-bootstrap for Catalog

--- components/playable-game-card.tsx
+++ components/playable-game-card.tsx
@@ -2,13 +2,17 @@ import Link from 'next/link'
 import styles from './cardboard.module.css'
 import React from 'react'
 
+import Card from 'react-bootstrap/Card'
+
 import { Pack } from '../types/game'
+
+import Button from 'react-bootstrap/Button'
 
 interface Props {
   pack: Pack
 }
 
-const PlayableGameCard = (props: Props) => {
+const PlayableGameCard_old = (props: Props) => {
   return (
     <div className={styles['cardboard']}>
       <img className={styles['poster-img']} src='https://i.ytimg.com/vi/IWyS18Yf9J0/hqdefault.jpg' />
@@ -22,18 +26,28 @@ const PlayableGameCard = (props: Props) => {
             <span className={styles['game-maker']}>{props.pack.maker}</span>
           </div>
           <Link href='/playing'>
-            <button className={styles['btn-play']}>
+            <Button>
               {/* <img src="https://www.freepnglogos.com/uploads/play-button-png/icon-png-play-button-icons-and-png-backgrounds-24.png" /> */}
               <span>PLAY GAME</span>
-            </button>
+            </Button>
           </Link>
-          {/* <div> */}
-          {/* <span>2</span> */}
-          {/* <span>P</span> */}
-          {/* </div> */}
         </div>
       </div>
     </div>
+  )
+}
+
+const PlayableGameCard = (props: Props) => {
+  return (
+    <Card style={{ width: '15rem' }}>
+      <Card.Img variant='top' src='https://i.ytimg.com/vi/IWyS18Yf9J0/hqdefault.jpg' />
+      <Card.Body>
+        <Card.Title style={{ fontSize: '1.2em' }}>{props.pack.title}</Card.Title>
+        <Card.Text>{props.pack.desc}</Card.Text>
+        <Card.Text>{props.pack.maker}</Card.Text>
+        <Button variant='primary'>Play Game</Button>
+      </Card.Body>
+    </Card>
   )
 }
 
