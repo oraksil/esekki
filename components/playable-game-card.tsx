0a2643576eb5dc9df51979b7df9876929ded295e
gamz <gamzabaw@gmail.com>
2020-10-25 15:51:41 +0900

    Handle new player registration on game start at catalog

--- components/playable-game-card.tsx
+++ components/playable-game-card.tsx
@@ -1,5 +1,3 @@
-import Link from 'next/link'
-import styles from './cardboard.module.css'
 import React from 'react'
 
 import Card from 'react-bootstrap/Card'
@@ -10,42 +8,21 @@ import Button from 'react-bootstrap/Button'
 
 interface Props {
   pack: Pack
-}
-
-const PlayableGameCard_old = (props: Props) => {
-  return (
-    <div className={styles['cardboard']}>
-      <img className={styles['poster-img']} src='https://i.ytimg.com/vi/IWyS18Yf9J0/hqdefault.jpg' />
-
-      <div>
-        <div className={styles['pack-profile']}>
-          <div>
-            <span className={styles['game-title']}>{props.pack.title}</span>
-          </div>
-          <div>
-            <span className={styles['game-maker']}>{props.pack.maker}</span>
-          </div>
-          <Link href='/playing'>
-            <Button>
-              {/* <img src="https://www.freepnglogos.com/uploads/play-button-png/icon-png-play-button-icons-and-png-backgrounds-24.png" /> */}
-              <span>PLAY GAME</span>
-            </Button>
-          </Link>
-        </div>
-      </div>
-    </div>
-  )
+  onStart: (pack: Pack) => void
 }
 
 const PlayableGameCard = (props: Props) => {
+  const { pack } = props
   return (
     <Card style={{ width: '15rem' }}>
       <Card.Img variant='top' src='https://i.ytimg.com/vi/IWyS18Yf9J0/hqdefault.jpg' />
       <Card.Body>
-        <Card.Title style={{ fontSize: '1.2em' }}>{props.pack.title}</Card.Title>
-        <Card.Text>{props.pack.desc}</Card.Text>
-        <Card.Text>{props.pack.maker}</Card.Text>
-        <Button variant='primary'>Play Game</Button>
+        <Card.Title style={{ fontSize: '1.2em' }}>{pack.title}</Card.Title>
+        <Card.Text>{pack.desc}</Card.Text>
+        <Card.Text>{pack.maker}</Card.Text>
+        <Button variant='primary' onClick={() => props.onStart(pack)}>
+          Play Game
+        </Button>
       </Card.Body>
     </Card>
   )
